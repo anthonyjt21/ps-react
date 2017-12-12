7dcc61fb124fcd6947115bc5678146d57d1add85
HP <hp@gmail.com>
2017-12-11 20:52:37 -0600

    ps react

---
+++ src/docs/examples/HelloWorld/ExampleHelloWorld.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import HelloWorld from 'ps-react/HelloWorld';
+/*import HelloWorld from '../../../components/HelloWorld/HelloWorld'*/
+
+/** Custom message */
+export default function ExampleHelloWorld() {
+  return <HelloWorld message="Pluralsight viewers!" />
+}
